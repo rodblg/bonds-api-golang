40b5a839308053f4964e6211111c74183ad17466
rodblg <rodrigoblancas19@gmail.com>
2024-06-22 20:33:32 -0600

    feature: Add provisional user mongo model

---
+++ pkg/database/user.go
@@ -0,0 +1,12 @@
+package database
+
+// type UserMongo struct {
+// }
+
+// type User struct {
+// 	Name     string
+// 	LastName string
+// 	Email    string
+// 	Password string
+// 	Bonds    []Bond
+// }
