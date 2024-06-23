55cd9c17fb2eecaace748be3c444fa0083ed858b
rodblg <rodrigoblancas19@gmail.com>
2024-06-22 20:25:44 -0600

    feat: Add function to insert new bonds to interact with db

--- pkg/usecases/usecases.go
+++ pkg/usecases/usecases.go
@@ -1,13 +1,31 @@
 package usecases
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"log"
+
+	"github.com/rodblg/bonds-api-golang/pkg/bondApi"
+	"github.com/rodblg/bonds-api-golang/pkg/database"
+)
 
 type UsecasesController struct {
-	Db mongo.Database
+	Storage *database.MongoController
+	//Storage bondApi.Storage
 }
 
-func NewUsecasesController(database mongo.Database) *UsecasesController {
+func NewUsecasesController(s *database.MongoController) *UsecasesController {
+	//func NewUsecasesController(s bondApi.Storage) *UsecasesController {
 	return &UsecasesController{
-		Db: database,
+		Storage: s,
 	}
+}
+
+func (u *UsecasesController) SellNewBonds(data bondApi.Bond) error {
+
+	log.Println("entering usecases")
+	err := u.Storage.InsertNewData(data)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
