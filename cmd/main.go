5233857a47af67e69725c71e00eb32fdb92b0a5e
rodblg <rodrigoblancas19@gmail.com>
2024-06-24 15:28:25 -0600

    feat: Add initialSetup into the main entry point

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 
+	"github.com/rodblg/bonds-api-golang/pkg/auth"
+	"github.com/rodblg/bonds-api-golang/pkg/bondApi"
 	"github.com/rodblg/bonds-api-golang/pkg/database"
 	http "github.com/rodblg/bonds-api-golang/pkg/http"
 	"github.com/rodblg/bonds-api-golang/pkg/usecases"
@@ -21,6 +23,27 @@ func main() {
 	storage := database.NewMongoController(mongoDatabase, collectionName)
 
 	usecasesController := usecases.NewUsecasesController(storage)
+	initialMongoSetUp(usecasesController)
 
 	http.ListenAndServe(usecasesController)
+}
+
+func initialMongoSetUp(u *usecases.UsecasesController) {
+	user, err := u.GetUser("Rodrigo")
+	if err != nil {
+		log.Print(err)
+	}
+	if user == nil {
+		password := auth.HashPassword("testing")
+		initialUser := bondApi.User{
+			Name:     "Rodrigo",
+			LastName: "Blancas",
+			Email:    "rb12@email.com",
+			Password: password,
+		}
+		err := u.CreateUser(&initialUser)
+		if err != nil {
+			log.Println("error creating initial user")
+		}
+	}
 }
