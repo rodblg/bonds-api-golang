b4097dbd6069edf01d6de8573253991265953b65
rodblg <rodrigoblancas19@gmail.com>
2024-06-24 15:30:21 -0600

    feat: Add Authentication middleware handler function

---
+++ pkg/auth/middleware.go
@@ -0,0 +1,33 @@
+package auth
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+func Authentication() func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			clientToken := r.Header.Get("token")
+			if clientToken == "" {
+				w.WriteHeader(http.StatusUnauthorized)
+				fmt.Fprintf(w, "Unauthorized: No authorization header provided")
+				return
+			}
+			claims, err := ValidateToken(clientToken)
+			if err != nil {
+				w.WriteHeader(http.StatusUnauthorized)
+				fmt.Fprintf(w, "Unauthorized: Invalid token (%v)", err)
+				return
+			}
+			// Set user data on context (assuming claims has Email and Uid)
+			ctx := context.WithValue(r.Context(), "email", claims.Email)
+			ctx = context.WithValue(ctx, "id", claims.ID)
+			r = r.WithContext(ctx)
+
+			// Call the next handler in the chain
+			next.ServeHTTP(w, r)
+		})
+	}
+}
