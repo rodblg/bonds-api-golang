9af7feed4d2472814b83f52d8f88a68dd4bef568
rodblg <rodrigoblancas19@gmail.com>
2024-06-25 18:05:42 -0600

    refactor: Change mongo URI to access the mongo container from the api container

--- pkg/database/mongo.go
+++ pkg/database/mongo.go
@@ -28,7 +28,8 @@ func MongoConnection(database string) (*mongo.Database, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://root:example@localhost:27017"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://root:example@mongodb:27017/"))
+	//client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://root:example@localhost:27017"))
 	if err != nil {
 		log.Println("error while connecting...")
 		return nil, err
